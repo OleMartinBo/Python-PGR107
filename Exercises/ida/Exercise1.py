60e61949c8cd674f8710f2fdea106328aa7e8a63
idawaaler <Ida_waaler@live.no>
2023-01-20 17:08:39 +0100

    Excercise 1

---
+++ Exercises/ida/Exercise1.py
@@ -0,0 +1,38 @@
+
+#A 
+
+from cmath import sqrt
+
+
+x = 2.5
+y = -1.5
+m = 18
+n = 4
+
+#A
+
+
+print(x + n * y - (x + n) * y )
+
+#B
+print(m // n + m % n)
+
+#C
+print(5*x-n/5)
+
+#D
+print(1-(1-(1-(1-(1-n)))))
+
+#E
+oppE = sqrt(sqrt (n))
+print(oppE)
+
+print("----------")
+#Oppgave 2
+
+
+n = 17
+m = 18 
+
+#A
+
