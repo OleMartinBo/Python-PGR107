e3fc55ce34cf5653baab28585fd62620681fdfaf
Ole <olebor1999@hotmail.com>
2023-01-20 16:46:08 +0100

    oppgave a og b

--- Exercises/Ole/Exercise1.py
+++ Exercises/Ole/Exercise1.py
@@ -1,1 +1,16 @@
-import math 
+import math
+
+#Variabler
+x = 2.5
+y = -1.5
+m = 18
+n = 4
+spaceBetween = "----------"
+
+#A
+opA = x + n * y - (x+n) * y
+print("Answer: " + " " + str(opA))
+#B
+print(spaceBetween)
+opB = m // n + m % n
+print("Answer: " + " " + str(opB))
