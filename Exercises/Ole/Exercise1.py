9d404344debfb67db7fe911b07b24bfe0cba7705
Ole <olebor1999@hotmail.com>
2023-01-20 16:54:52 +0100

    opp 1

--- Exercises/Ole/Exercise1.py
+++ Exercises/Ole/Exercise1.py
@@ -7,10 +7,20 @@ m = 18
 n = 4
 spaceBetween = "----------"
 
+#Oppgave 1
 #A
 opA = x + n * y - (x+n) * y
-print("Answer: " + " " + str(opA))
+print("Answer a: " + " " + str(opA))
 #B
 print(spaceBetween)
 opB = m // n + m % n
-print("Answer: " + " " + str(opB))
+print("Answer b: " + " " + str(opB))
+#C
+print(spaceBetween)
+opC = 5 * x - n / 5
+print("Answer C: " + " " + str(opC))
+#D
+print(spaceBetween)
+opD = 1 - (1-(-1(-1(1 - n))))
+print(opD)
+
