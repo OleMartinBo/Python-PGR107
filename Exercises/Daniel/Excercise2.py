fae5b9eb96d950ed664d3b945ff9309a2c38c789
hopedaniel01 <danielhope@hotmail.no>
2023-01-24 14:05:51 +0100

    Oppgave 2 start

--- Exercises/Daniel/Excercise2.py
+++ Exercises/Daniel/Excercise2.py
@@ -1,6 +1,71 @@
+from math import sqrt
+import sys
+
+
 n = 1
 m = -1
-if n < -m:
-    print(n)
+#if n < -m:
+#    print(n)
+#else:
+#    print(m)
+
+#if -n >= m:
+    #print(n)
+#else:
+    #print(m)
+    
+#x = 0.0
+#y = 1.0
+
+#if abs(x-y) < 1:
+ #   print(x)
+#else:
+ #   print(y)
+ 
+#x = sqrt(2.0)
+#y = 2.0
+ 
+#if x * x == y:
+#    print(x)
+#else:
+#    print(y)
+
+#userInput = int(input("Enter a number between 1 and 10: "))
+
+#if userInput < 1:
+#    print("You entered a number < 1")
+#elif userInput > 10:
+#    print("The number you entered is > 10")
+#else:
+#    print(userInput)
+
+userInput = input("Enter your grade number: ")
+
+if userInput.isdigit():
+    userInput = int(userInput)
+
+    if userInput < 1 or userInput > 100:
+        print("Please type a number larger than 1, and lower than 100")
+        sys.exit()
 else:
-    print(m)
+        print("Please type a number next time")
+        sys.exit()
+
+if userInput >= 90 and userInput <= 100:
+    print("A")
+elif userInput >= 80 and userInput <= 89:
+    print("B")
+elif userInput >= 70 and userInput <= 79:
+    print("C")
+elif userInput >= 60 and userInput <= 69:
+    print("D")
+elif userInput < 60:
+    print("F") 
+
+        
+        
+        
+    
+
+
+    
