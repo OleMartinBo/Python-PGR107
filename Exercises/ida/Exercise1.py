4e692046faa8cb8d7fc8d9eb7c3ed27b1e5f1205
idawaaler <Ida_waaler@live.no>
2023-01-31 10:38:22 +0100

    lecture and exercise1

--- Exercises/ida/Exercise1.py
+++ Exercises/ida/Exercise1.py
@@ -1,42 +1,152 @@
 
 
-print("----- oppgave 1 -----")
+# print("----- oppgave 1 -----")
 
-from cmath import sqrt
+# from cmath import sqrt
 
 
-x = 2.5
-y = -1.5
-m = 18
-n = 4
+# x = 2.5
+# y = -1.5
+# m = 18
+# n = 4
 
-#A
+# #A
 
-print(x + n * y - (x + n) * y )
+# print(x + n * y - (x + n) * y )
 
-#B
-print(m // n + m % n)
+# #B
+# print(m // n + m % n)
 
-#C
-print(5*x-n/5)
+# #C
+# print(5*x-n/5)
 
-#D
-print(1-(1-(1-(1-(1-n)))))
+# #D
+# print(1-(1-(1-(1-(1-n)))))
 
-#E
-oppE = sqrt (n)
-print(oppE)
+# #E
+# oppE = sqrt (n)
+# print(oppE)
 
-print("-----avslutt oppgave 1 -----")
+# print("-----Slutt på oppgave 1 -----")
 
 
 #Oppgave 2
 
-print("----- oppgave 2 -----")
+# from re import S
 
 
-n = 17
-m = 18 
+# print("----- oppgave 2 -----")
 
-#A
 
+# n = 17
+# m = 18 
+
+# #A
+# print(n // 10 + n % 10)
+
+# #B
+# print(n % 2 + m % 2)
+
+# #C
+# print((m+n)//2)
+
+# #D
+# print((m+n) / 2.0)
+
+# #E
+# print(int(0.5*(m+n)))
+
+# #F
+# print(int(round(0.5 *(m+n))))
+
+# print("----- Slutt på oppgave 2 -----")
+
+# #Oppgave 3
+# print("----- oppgave 3 -----")
+
+# s= "Hello"
+# t = "World"
+
+# #A
+# print(len(s) + len(t))
+
+# #B
+# print(s[1] + s[2])
+
+# #C
+# print(s[len (s) // 2])
+
+# #D
+# print(s + t)
+
+# #E
+# print(t + s)
+
+# #F
+# print(s * 2)
+
+# # print("----- Slutt på oppgave 3-----")
+
+
+#Oppgave 4 
+
+# n1 = int(input("Enter number 1: "))
+# n2 = int(input("Enter number 2: "))
+
+# print(f"You have entered {n1} as number 1, and {n2} as number 2. The result will then be: ")
+
+# print("Sum of the two numbers:", n1+n2)
+# print("Difference between the numbers:", n1-n2)
+# print("Product:", n1*n2)
+# print("The average:", (n1+n2)/2)
+# print("Distance:", abs(n1-n2))
+
+# maxNumber = max(n1, n2)
+# print("Maximum number:", maxNumber)
+
+# minNumber = min(n1, n2)
+# print("Minimum number:", minNumber)
+
+
+#Oppgave 5
+
+# print("______________________")
+# n1 = int(input("Enter number 1: "))
+# n2 = int(input("Enter number 2: "))
+
+# print("\n")
+# print("%-15s" % "Sum:", n1+n2)
+# print("%-15s" % "Difference:", n1-n2)
+# print("%-15s" % "Product:", n1*n2)
+# print("%-15s" % "Average:", (n1+n2)/2)
+# print("%-15s" % "Distance:", abs(n1-n2))
+# print("%-15s" % "Max:", max(n1, n2))
+# print("%-15s" % "Min:", min(n1, n2))
+# print("______________________")
+
+#Oppgave 6
+
+# length = int(input("Enter length of rectangle: "))
+# width = int(input("Enter width of rectangle: "))
+
+# area = length * width
+# perimeter = 2*(length + width)
+
+# print("____________Result_______________")
+# print("The area of the rectangle:", area)
+# print("The perimeter of the rectangle:", perimeter)
+# print("_________________________________")
+
+
+#Oppgave 7
+
+# word = "kattepus"
+
+# lengthOfWord = len(word)
+
+# print(word[0] + word[1] + "..."+ word[lengthOfWord-2] + word[lengthOfWord-1])
+
+#Oppgave 8
+
+digits = input("Enter a positive number with five digits: ")
+print(digits[0], digits[1],digits[2], digits[3], digits[4])
---
+++ Exercises/ida/Lecture.py
@@ -0,0 +1,138 @@
+# CODE IN LECTURES 
+# 
+# LECTURE 3: DECISIONS 
+
+#compare - discount
+#compute the discount for a given purchase 
+
+originalPrice = float(input("Original price before discount: "))
+
+if originalPrice < 128:
+    discountRate = 0.92
+else:
+    discountRate = 0.84
+
+discountPrice = discountRate * originalPrice
+print("Discounted price: %.2f" % discountPrice)
+
+
+#Even - Odd
+
+a = int(input("Enter a number: "))
+
+if a % 2 == 1:
+    print(a, "is an odd number.")
+else:
+    print(a, "is an Even number.")
+
+#Middle charater in string
+
+string = input("Enter a string:")
+
+position = len(string) // 2
+
+if len(string) % 2 == 1:
+    result = string[position]
+else:
+    result = string[position-1] + string[position]
+
+print("Middle charecters in '%s' is '%s'" % (string, result))
+
+
+#Tax Calculation
+
+RATE1 = 0.10
+RATE2 = 0.25
+RATE1_SINGLE_LIMIT = 32000
+RATE1_MARRIED_LIMIT = 64000
+
+income = float(input("Please enter your income: "))
+marital_status = input("Please enter s for single, m for married: ")
+
+tax1 = 0
+tax2 = 0
+
+if marital_status == "s":
+    if income <= RATE1_SINGLE_LIMIT:
+        tax1 = RATE1 * income
+    else: 
+        tax1 = RATE1 * RATE1_SINGLE_LIMIT
+        tax2 = RATE2 * (income - RATE1_SINGLE_LIMIT)
+
+else: 
+    if income <= RATE1_MARRIED_LIMIT:
+        tax1 = RATE1 * income
+    else:
+        tax1 = RATE1 * RATE1_MARRIED_LIMIT
+        tax2 = RATE2 * (income - RATE1_MARRIED_LIMIT)
+
+total_tax = tax1 + tax2
+print("the tax is $%.2f" % total_tax)
+
+
+#VALIDATION userInput
+
+#number
+user_input = input("Enter a number: ")
+
+if user_input.isdigit():
+    number = int(user_input)
+    print(number)
+else:
+    print("you did not enter a number")
+
+#Floating point number
+user_input = input("Enter a floating point number: ")
+
+if user_input.replace(".", "").isdigit():
+    number = float(user_input)
+    print(number)
+else:
+    print("Something")
+
+
+#Richter Scale 
+# This program prints a description of an earthquake, given the Richter scale 
+# magnitude.
+#
+richter = float(input("Enter a magnitude on the Richter scale: "))
+
+if richter >= 8.0:
+    print("Most structures fall")
+elif richter >= 7.0:
+    print("Many buildings destroyed")
+elif richter >= 6.0:
+    print("Many buildings considerably damaged, some collapse")
+elif richter >= 4.5:
+    print("Damage to poorly constructed buildings")
+else :
+    print("No destruction of buildings")
+
+
+#Substrings
+
+theString = input("Enter a string: ")
+theSubString = input("Enter a substring: ")
+
+if theSubString in theString:
+    print("The string does contain the substring")
+
+    howMany = theString.count(theSubString)
+    print("It containts", howMany, "instances")
+
+    where = theString.find(theSubString)
+    print("The first occurence starts at position", where)
+
+    if theString.startswith(theSubString):
+        print("the string strarts with the substring.")
+    else:
+        print("the string does not starts with the substring.")
+
+    if theString.endswith(theSubString):
+        print("The string ends with the substring.")
+    else:
+        print("the string does not end with the substring.")
+
+else:
+    print("The string does not contain the substring.")
+
