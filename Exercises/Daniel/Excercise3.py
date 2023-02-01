a8aae77c65cb99c0ed2967e1f69739e330a87c53
hopedaniel01 <91459762+hopedaniel01@users.noreply.github.com>
2023-02-01 15:15:34 +0100

    Starten av leksen

---
+++ Exercises/Daniel/Excercise3.py
@@ -0,0 +1,71 @@
+# A
+
+# i = 0
+# j = 10
+# n = 0 
+
+# linebreaker = "-------------------------------------"
+
+# print(linebreaker)
+# print("%5s" % "i", "%5s" % "|", "%5s" % "j", "%5s" % "|", "%5s" % "n",  "%5s" % "|")
+# print(linebreaker)
+# while i < j : 
+#      i = i + 1
+#      j = j - 1
+#      n = n + 1
+#      print("%5s" % str(i), "%5s" % "|", "%5s" % str(j), "%5s" % "|", "%5s" % str(n), "%5s" % "|")
+#      print(linebreaker)
+
+#B
+
+# i = 0
+# j = 0
+# n = 0 
+
+# linebreaker = "-------------------------------------"
+
+# print(linebreaker)
+# print("%5s" % "i", "%5s" % "|", "%5s" % "j", "%5s" % "|", "%5s" % "n",  "%5s" % "|")
+# print(linebreaker)
+# while i < 10 : 
+#     i = i + 1
+#     n = n + i + j
+#     j = j + 1
+#     print("%5s" % str(i), "%5s" % "|", "%5s" % str(j), "%5s" % "|", "%5s" % str(n), "%5s" % "|")
+#     print(linebreaker)
+
+#C
+
+# i = 10
+# j = 0
+# n = 0 
+
+# linebreaker = "-------------------------------------"
+
+# print(linebreaker)
+# print("%5s" % "i", "%5s" % "|", "%5s" % "j", "%5s" % "|", "%5s" % "n",  "%5s" % "|")
+# print(linebreaker)
+# while i > 0 : 
+#     i = i - 1
+#     j = j + 1
+#     n = n + i - j
+#     print("%5s" % str(i), "%5s" % "|", "%5s" % str(j), "%5s" % "|", "%5s" % str(n), "%5s" % "|")
+#     print(linebreaker)
+
+#D
+
+# i = 0
+# j = 10
+# n = 0 
+
+# linebreaker = "-------------------------------------"
+
+# print(linebreaker)
+# print("%5s" % "i", "%5s" % "|", "%5s" % "j", "%5s" % "|", "%5s" % "n",  "%5s" % "|")
+# print(linebreaker)
+# while i != j : 
+#     i = i + 2
+#     j = j - 2
+#     n = n + 1
+#     print("%5s" % str(i), "%5s" % "|", "%5s" % str(j), "%5s" % "|", "%5s" % str(n), "%5s" % "|")
+#     print(linebreaker)
