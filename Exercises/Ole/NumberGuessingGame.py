29a5dae0510db6db177be2a1962d91fbc01053df
Ole Martin B <86748053+OleMartinBo@users.noreply.github.com>
2023-03-04 00:06:44 +0100

    Number game func1 ferdig

--- Exercises/Ole/NumberGuessingGame.py
+++ Exercises/Ole/NumberGuessingGame.py
@@ -1,29 +1,46 @@
 from random import randint
 
+
+def game() :
+    
 #Variabler 
-value = randint(1,1000)
-counter = 0
-
-
-
-
-
-while counter != 5 :
+    value = randint(1,1000)
+    counter = 0
     
-    userIput = int(input("Enter a number: "))
-    
-    if userIput == value :
-        print("Congratulation! You guessed the number in:", counter ,"attempt(s)”",)
-    elif userIput < value :
-        print("The number is Higher")
-    elif userIput > value :
-        print("The number is lower")
+#Logikk
+    while counter != 5 :
         
-    counter += 1
-    print("You got:", counter, "Wrong")
-
-print("The number was:", value)
+        userIput = int(input("Enter a number: "))
+        
+        if userIput == value :
+            print("Congratulation! You guessed the number in:", counter ,"attempt(s)”")
+            print("------------------------------------------------------------------")
+            print(" ")
+            break
+        elif userIput < value :
+            print("The number is Higher")
+            print("-------------------")
+            print(" ")
+        elif userIput > value :
+            print("The number is lower")
+            print("-------------------")
+            print(" ")
         
             
-    
+        counter += 1
+        if counter == 5 :
+            print("Sorry!! You did not manage to guess the number.\nYou have reached the guessing limit.\nThe number was:", value)
+            print("-------------------------------------------------")
+            print(" ")
+            
+            
 
+
+game()
+
+def menu():
+    choice = str(input("Enter YES to play again or NO to quit: "))
+    
+    while choice.lower or choice.upper == "yes" :
+        game()
+    
