4b307ab62eebba65c336fcd1971bfc89bf15ba9e
Ole Martin B <86748053+OleMartinBo@users.noreply.github.com>
2023-03-15 22:08:06 +0100

    Oppg 6 ex 7

--- Exercises/Ole/Exercise6.py
+++ Exercises/Ole/Exercise6.py
@@ -111,3 +111,36 @@ def sequence() :
     print("The values from lowest to highest:",values)
 
 #sequence()
+
+#Oppgave 7
+def listItems():
+    listOne = [1 , 4,  9,  16,  9,  7,  4,  9,  11 ]
+    listTwo = [11,  11,  7,  9,  16,  4,  1 ]
+
+    same_list(listOne, listTwo)
+
+def same_list(a, b) :
+    
+    removeItem(a)
+    removeItem(b)
+    
+    if len(a) != len(b) :
+        print(a, "is not identical to", b)
+    else :
+        identical = True
+        
+        if identical :
+            print(a, "And", b, "Are the same")
+        else :
+            print(a, "And", b, "Are not the same")
+
+
+def removeItem(myList): 
+    for item in myList :
+        count = myList.count(item)
+        if count > 1 :
+            for j in range(count -1) :
+                myList.remove(item)
+    
+    
+listItems()
