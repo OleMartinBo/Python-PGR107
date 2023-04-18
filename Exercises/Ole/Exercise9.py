5ad7bced9514130599bc84fbfa0cf89f9e816f6a
Ole Martin B <86748053+OleMartinBo@users.noreply.github.com>
2023-04-18 17:39:35 +0200

    Ex9

--- Exercises/Ole/Exercise9.py
+++ Exercises/Ole/Exercise9.py
@@ -40,31 +40,165 @@ def oppgave1():
 
 #Oppgave 2
 class Address:
-    def __init__(self, house_number, street, city, state, postal_code):
+    def __init__(self, house_number, street, city, state, postal_code, apartment_number = 0):
         self.house_number = house_number
         self.street = street
         self.city = city
         self.state = state
         self.postal_code = postal_code
-        self.apartment_number = None
-        
-    def set_aptNumb(self, apartment_number=""):
         self.apartment_number = apartment_number
         
+    def get_postal_code(self):
+        return self.postal_code
+        
     def print_address(self):
-        if self.apartment_number is not None:
-            print(f"{self.house_number} {self.street}, Apt {self.apartment_number}")
-        else:
-            print(f"{self.house_number} {self.street}")
-        print(f"{self.city}, {self.state} {self.postal_code}")
+        if self.apartment_number == 0:
+            return f"House number: {self.house_number}, Street: {self.street}\nCity: {self.city}, State: {self.state}, Postal code: {self.postal_code}"
+        
+        return f"Street: {self.street}, House Number: {self.house_number}, Apratment Number: {self.apartment_number}\nCity: {self.city}, State: {self.state}, Postal Code: {self.postal_code}"
 
     def comes_before(self, other):
-        return self.postal_code < other.postal_code
+        if self.get_postal_code() < other.get_postal_code():
+            return True
+        return False        
+    
+def oppgave2():
+    p1 = Address("Oslo", "Oslo", "0645", "Dronningsgata", 18)
+    print(p1.print_address())
+    p2 = Address("Stavanger", "Rogaland", "3657", "Holbergsgate", 33, 8976)
+    print(p2.print_address())
+    print(p1.comes_before(p2))
+
+#oppgave2()
+
+
+class Car:
+    def __init__(self, fuel_efficieny, fuel = 0):
+        self.fuel = fuel
+        self.fuel_efficieny = fuel_efficieny
+        
+    def getGas(self):
+        return self.fuel
+    
+    def addGas(self, amount):
+        self.fuel = self.fuel + amount
+        
+    def moveCar(self, distance):
+        if self.fuel * self.fuel_efficieny < distance:
+            print("Cant drive with no fuel")
+            return False
+        
+        self.fuel = self.fuel - distance/self.fuel_efficieny
+        return True
+    
+def oppgave3():
+    
+    myHybrid = Car (50)
+    myHybrid.addGas (20) 
+    myHybrid.moveCar (100) 
+    print(myHybrid.getGas()) 
+
+#oppgave3()
+
+
+class Letter:
+    def __init__(self, letterFrom, letterTo):
+        self.letterFrom = letterFrom
+        self.letterTo = letterTo
+        self.text = ""
+        
+    def addLine(self, line):
+        self.text = self.text + line + "\n"
+    
+    def getText(self):
+        textOutput = "Dear: " + self.letterTo + "\n\n"
+        textOutput = textOutput + self.text 
+        textOutput = textOutput  + "\n"+ "Sincerly\n\n" + self.letterFrom
+        return textOutput
     
 
-p1 = Address(12,"gjerdumsvei","oslo","nogre", 0)
+def oppgave4():
+    my_letter = Letter("Marry", "John")
+    my_letter.addLine("I am sorry we must part.")
+    my_letter.addLine("I wish you all the best.")
+    print(my_letter.getText())
 
-p1.set_aptNumb("G")
+#oppgave4()
 
-p1.print_address()
+class Customer:
+    def __init__(self):
+        self.totalPurchases = 0
+    
+    def getTotal(self):
+        return self.totalPurchases
+    
+    def makePurchase(self,amount):
+        if self.discountReached() :
+            self.totalPurchases = self.totalPurchases + amount - 10
+        else :
+            self.totalPurchases = self.totalPurchases + amount
+    
+    def discountReached(self):
+        if self.totalPurchases >= 100 :
+            return True
+        return False
+    
+def oppgave5():
 
+    cust1 = Customer()
+    
+    cust1.makePurchase(102)
+    print(cust1.getTotal())
+    
+    cust1.makePurchase(4)
+    print(cust1.getTotal())
+    
+    cust1.makePurchase(3)
+    print(cust1.getTotal())
+    
+#oppgave5()
+
+    
+    
+class Bank:
+    def __init__(self, balance = 0):
+        self.balance = balance
+        
+    def getBalance(self) :
+        return self.balance
+    
+    def deposit(self, amount) :
+        self.balance = self.balance + amount
+        
+    def withdraw(self,amount) :
+        PENALTY = 10
+        if amount > self.balance :
+            self.balance = self.balance - PENALTY
+        else:
+            self.balance = self.balance -amount
+            
+    def addInterest(self, rate) :
+        amount = self.balance * rate / 100
+        self.balance = self.balance + amount
+        
+        
+        
+def oppgave6():
+    
+    accountCheck = Bank()
+    
+    accountCheck.deposit(2500)
+    print(accountCheck.getBalance())
+    accountCheck.withdraw(500)
+    print(accountCheck.getBalance())
+    accountCheck.addInterest(2)
+    print(accountCheck.getBalance())
+    
+oppgave6()
+        
+        
+    
+    
+    
+    
+    
